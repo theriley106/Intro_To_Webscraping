586d003b3423524048765abce4ad1402e6c0dcbc
Theriley106 <christopherlambert106@gmail.com>
2019-01-26 18:07:01 -0500

    Added main2

---
+++ main2.py
@@ -0,0 +1,35 @@
+import requests
+import bs4
+import RandomHeaders
+import re
+
+URL = "https://www.amazon.com/s/ref=nb_sb_ss_c_2_17?url=search-alias%3Dstripbooks&field-keywords={0}"
+SELECTOR = ".s-item-container"
+PRICE_CSS = ".a-size-base.a-color-base"
+TRADE_SELECTOR = ".a-text-normal .a-size-small .a-color-base"
+URL.format("biology+textbooks")
+
+def create_url(keyword, page=1):
+	return URL.format(keyword.replace(" ", "+")) + "&page={}".format(page)
+
+def get_url(url):
+	headers = RandomHeaders.LoadHeader()
+	return requests.get(url, headers=headers)
+
+def string_to_float(string):
+	return float('.'.join(re.findall("\d+", string)))
+
+
+if __name__ == '__main__':
+	urlVal = create_url("Biology Textbooks")
+	res = get_url(urlVal)
+	page = bs4.BeautifulSoup(res.text, 'lxml')
+	#print(page.title.string)
+	selections = page.select(SELECTOR)
+	for item in selections:
+		try:
+			priceVal = (string_to_float(item.select(PRICE_CSS)[0].getText()))
+			tradeVal = (string_to_float(item.select(TRADE_SELECTOR)[0].getText()))
+			print(tradeVal - priceVal)
+		except:
+			pass
