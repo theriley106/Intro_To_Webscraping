4eb68a689d6490437778852cfbee45dbe6c72e8e
Theriley106 <christopherlambert106@gmail.com>
2019-01-26 11:16:57 -0500

    Added textbook trade in

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ SECTION_CSS = ".a-fixed-left-grid-inner"
 TEXTBOOK_CSS = ".a-text-normal .a-size-small .a-color-base"
 PRICE_CSS = ".a-size-base.a-color-base"
 
-def gen_url(keyword):
-	return URL_FORMAT.format(keyword.replace(" ", "+"))
+def gen_url(keyword, page=1):
+	url = URL_FORMAT.format(keyword.replace(" ", "+"))
+	if page > 1:
+		url += '&page={}'.format(page)
+	return url
 
 def grab_site(url):
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 	return requests.get(url, headers=headers)
 
 def convert_string_to_float(stringVal):
-	if '$' in stringVal and '.' in stringVal:
+	try:
 		price = stringVal.replace("$", "")
-		try:
-			price = float(price)
-			return price
-		except:
-			return None
+		price = float(price)
+		return price
+	except:
+		return None
 
 
 if __name__ == '__main__':
@@ -29,16 +31,23 @@ if __name__ == '__main__':
 	res = grab_site(url)
 	page = bs4.BeautifulSoup(res.text, 'lxml')
 	print page.title.string
-	for section in page.select(SECTION_CSS):
-		priceVal = section.select(PRICE_CSS)
-		tradeInPrice = section.select(TEXTBOOK_CSS)
-		if len(priceVal) == 0 or len(tradeInPrice) == 0:
-			continue
-		priceVal = priceVal[0].getText()
-		tradeInPrice = tradeInPrice[0].getText()
-		if priceVal == None or tradeInPrice == None:
-			continue
-		priceVal = convert_string_to_float(priceVal)
-		tradeInPrice = convert_string_to_float(tradeInPrice)
-		print("Price: {}".format(priceVal))
-		print("Trade In Price: {}".format(tradeInPrice))
+	pageNumbers = int(page.select(".pagnDisabled")[0].getText())
+	#for pageNum in range(1, pageNumbers):
+	for i in range(2,pageNumbers+1):
+		for section in page.select(SECTION_CSS):
+			priceVal = section.select(PRICE_CSS)
+			tradeInPrice = section.select(TEXTBOOK_CSS)
+			if len(priceVal) == 0 or len(tradeInPrice) == 0:
+				continue
+			priceVal = priceVal[0].getText()
+			tradeInPrice = tradeInPrice[0].getText()
+			priceVal = convert_string_to_float(priceVal)
+			tradeInPrice = convert_string_to_float(tradeInPrice)
+			if priceVal == None or tradeInPrice == None:
+				continue
+			profit = tradeInPrice - priceVal
+			print("Price: {} | Trade In Price: {} | Profit: {}".format(priceVal, tradeInPrice, profit))
+		url = gen_url(keyWord, i)
+		res = grab_site(url)
+		page = bs4.BeautifulSoup(res.text, 'lxml')
+		print("{} | Page: {}".format(page.title.string, i))
