7fc7a9e0cb38dd4c461316323ad33b32d1be57bb
Christopher Lambert <christopherlambert106@gmail.com>
2019-07-02 10:14:11 -0400

    Updated scraper to work properly

--- main2.py
+++ main2.py
@@ -1,6 +1,6 @@
 import requests
 import bs4
-import RandomHeaders
+import randomheaders
 import re
 
 URL = "https://www.amazon.com/s/ref=nb_sb_ss_c_2_17?url=search-alias%3Dstripbooks&field-keywords={0}"
@@ -13,7 +13,7 @@ def create_url(keyword, page=1):
 	return URL.format(keyword.replace(" ", "+")) + "&page={}".format(page)
 
 def get_url(url):
-	headers = RandomHeaders.LoadHeader()
+	headers = randomheaders.LoadHeader()
 	return requests.get(url, headers=headers)
 
 def string_to_float(string):
@@ -24,12 +24,13 @@ if __name__ == '__main__':
 	urlVal = create_url("Biology Textbooks")
 	res = get_url(urlVal)
 	page = bs4.BeautifulSoup(res.text, 'lxml')
-	#print(page.title.string)
+	print(page.title.string)
 	selections = page.select(SELECTOR)
 	for item in selections:
 		try:
 			priceVal = (string_to_float(item.select(PRICE_CSS)[0].getText()))
 			tradeVal = (string_to_float(item.select(TRADE_SELECTOR)[0].getText()))
 			print(tradeVal - priceVal)
-		except:
+		except Exception as exp:
+			print(exp)
 			pass
