59ae6a014352c32790a7c6dc7bc9a5441d6bbbd4
Christopher Lambert <christopherlambert106@gmail.com>
2019-07-03 12:19:31 -0400

    Set up script works properly

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 sudo -H python -m ensurepip
 cp -r dependencies/* .
-python -m pip config set global.index-url https://artifactory.cloud.capitalone.com/artifactory/api/pypi/pypi-internalfacing/simple/
+mkdir -p ~/.pip
+cat > ~/.pip/pip.conf << EOF
+[global]
+timeout = 60
+index-url = https://artifactory.cloud.capitalone.com/artifactory/api/pypi/pypi-internalfacing/simple
+EOF
 python -m pip install -r requirements.txt -t .
 python -c 'import randomheaders;import bs4;import requests;import lxml;page = bs4.BeautifulSoup("<html></html>", "lxml");'
