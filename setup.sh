a7c211deb0c8ed42b175d0f32e4a655dc581dab5
Christopher Lambert <christopherlambert106@gmail.com>
2019-07-03 11:31:31 -0400

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+sudo -H python -m ensurepip
 cp -r dependencies/* .
-pip config set global.index-url https://artifactory.cloud.capitalone.com/artifactory/api/pypi/pypi-internalfacing/simple/
-pip install lxml -t .
+python -m pip config set global.index-url https://artifactory.cloud.capitalone.com/artifactory/api/pypi/pypi-internalfacing/simple/
+python -m pip install -r requirements.txt -t .
 python -c 'import randomheaders;import bs4;import requests;import lxml;page = bs4.BeautifulSoup("<html></html>", "lxml");'
